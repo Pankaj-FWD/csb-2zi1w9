3e338ff6573b5c4e0fadfd3e56a1a1d0a2ab7753
Code Sandbox <hello@codesandbox.io>
2023-02-05 12:40:17 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.72edc1dba0d52c4d7690214af5915255.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "967fea04e480a9bcae4ca383279bfa12",
+    "url": "https://pankaj-fwd.github.io/csb-2zi1w9/index.html"
+  },
+  {
+    "revision": "eb226c0a07784c72e387",
+    "url": "https://pankaj-fwd.github.io/csb-2zi1w9/static/css/2.d8c133cf.chunk.css"
+  },
+  {
+    "revision": "1f40ba1ebfd4403bc8b0",
+    "url": "https://pankaj-fwd.github.io/csb-2zi1w9/static/css/main.ea585a19.chunk.css"
+  },
+  {
+    "revision": "eb226c0a07784c72e387",
+    "url": "https://pankaj-fwd.github.io/csb-2zi1w9/static/js/2.da8f56d0.chunk.js"
+  },
+  {
+    "revision": "1f40ba1ebfd4403bc8b0",
+    "url": "https://pankaj-fwd.github.io/csb-2zi1w9/static/js/main.dbea23fb.chunk.js"
+  },
+  {
+    "revision": "1294482a78e1e6f1524e",
+    "url": "https://pankaj-fwd.github.io/csb-2zi1w9/static/js/runtime~main.853651b8.js"
+  }
+]);
